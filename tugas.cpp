5acc3c1528fbf11a6158657e90095419b46dc19e
muhammadfaatihaflah-del <muhammadfaatihaflah@gmail.com>
2026-04-27 11:32:48 +0700

    membuat class layang layang

--- tugas.cpp
+++ tugas.cpp
@@ -4,3 +4,37 @@ using namespace std;
 
 // Forward declaration
 class BelahKetupat;
+
+class LayangLayang {
+private:
+    float d1, d2; // diagonal
+    float s1, s2; // sisi
+
+public:
+    void input() {
+        cout << "=== Layang-Layang ===" << endl;
+        cout << "Masukkan diagonal 1: ";
+        cin >> d1;
+        cout << "Masukkan diagonal 2: ";
+        cin >> d2;
+        cout << "Masukkan sisi 1: ";
+        cin >> s1;
+        cout << "Masukkan sisi 2: ";
+        cin >> s2;
+    }
+     float luas() {
+        return 0.5 * d1 * d2;
+    }
+
+    float keliling() {
+        return 2 * (s1 + s2);
+    }
+
+    void output() {
+        cout << "Luas Layang-Layang: " << luas() << endl;
+        cout << "Keliling Layang-Layang: " << keliling() << endl;
+    }
+
+    // friend function dari class BelahKetupat
+    friend float hitungKelilingBelahKetupat(BelahKetupat b);
+};
